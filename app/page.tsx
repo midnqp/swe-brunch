e7ce5320b63b9ac341cb879294cdd4f9ebd64877
midnqp <midnqp@gmail.com>
2025-11-13 13:00:05 +0600

    feat: add pagelayout and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,50 @@
-import Image from "next/image";
+import { PageLayout } from "@/components/page";
+import Image from "next/image"
 
-export default function Home() {
+export default function BrowsePage() {
+  const food = {
+    name: "",
+    price: 0,
+    description:
+      "lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet",
+  }
+  const items: Array<{ name: string; price: number }> = Array(10)
+    .fill(food)
+    .map((f, i) => ({ ...f, name: `Item ${i + 1}`, price: (i + 1) * 100 }))
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <PageLayout>
+      <h1 className="mb-8 text-4xl">Browse</h1>
+      {/** tip: ideal layout for item menus */}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {items.map((item) => (
+          <Card {...item} />
+        ))}
+      </div>
+    </PageLayout>
+  )
+}
+
+
+function Card(props: any) {
+  const item = props
+  return (
+    <div className="border-gray-400 border-2 rounded-md p-4">
+      {/** tip: how to work with next's image compo. */}
+      <div className="relative w-full h-48">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          className=""
+          alt=""
+          src="/food.jpg"
+          layout="fill"
+          objectFit="contain"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
+      <div>
+        <p className="font-bold">{item.name}</p>
+        <p>${item.price}</p>
+        <p>{item.description}</p>
+      </div>
     </div>
-  );
+  )
 }
