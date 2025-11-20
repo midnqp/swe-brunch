b495bb345b6d5c11bee5eb816224cee933089b55
midnqp <midnqp@gmail.com>
2025-11-20 15:51:57 +0600

    chore: lint

--- hooks/useCartItems.tsx
+++ hooks/useCartItems.tsx
@@ -2,24 +2,26 @@ import { useGlobalState } from "@/stores/useGlobalState"
 import { useCallback, useEffect, useMemo, useRef, useState } from "react"
 
 export default function useCartItems() {
-  const [cartItems, setCartItems] = useState<null | Array<Record<string, any>>>(null)
+  const [cartItems, setCartItems] = useState<null | Array<Record<string, any>>>(
+    null,
+  )
   const prevCartItems = useRef<any>(null)
   // note: using selector to only get the setter function,
   // prevents a re-render caused by the globalState.cartItemsCount
-  // value's change. 
-  // 
+  // value's change.
+  //
   // now, alhamdulillah, this component only renders once when the add()
   // function is called.
-  const setCartItemsCount = useGlobalState(state => state.setCartItemsCount)
-  const globalState = {setCartItemsCount}
+  const setCartItemsCount = useGlobalState((state) => state.setCartItemsCount)
+  const globalState = { setCartItemsCount }
 
   useEffect(() => {
-    console.log('usecartitems: useeffect first render')
+    console.log("usecartitems: useeffect first render")
     const s = window.localStorage.getItem("cartItems") || "[]"
     const list = JSON.parse(s)
     setCartItems(list)
     prevCartItems.current = list
-    console.log('usecartitems: loaded cart from localstorage', list.length)
+    console.log("usecartitems: loaded cart from localstorage", list.length)
     globalState.setCartItemsCount(list.length)
   }, [])
 
@@ -30,30 +32,30 @@ export default function useCartItems() {
   // will see sad stale data.
   //
   // the ref check here is added to prevent the 1 redudant update
-  // to the localstorage as the 'cartitems' array is filled 
+  // to the localstorage as the 'cartitems' array is filled
   // from the localstorage for the first time by the empty-deps
   // effect.
   //
   // alhamdulillah. all works perfect.
   useEffect(() => {
     if (cartItems == null) return
-    if (prevCartItems.current == cartItems ) return
-    window.localStorage.setItem('cartItems', JSON.stringify(cartItems))
+    if (prevCartItems.current == cartItems) return
+    window.localStorage.setItem("cartItems", JSON.stringify(cartItems))
     globalState.setCartItemsCount(cartItems.length)
-    console.log('usecartitems: updating cart local storage', cartItems.length)
-    prevCartItems.current  = cartItems
+    console.log("usecartitems: updating cart local storage", cartItems.length)
+    prevCartItems.current = cartItems
   }, [cartItems])
 
   /**
    * note: the individual add and remove functions
    * have been wrapped in usecallback. otherwise when
    * this hook re-renders due to changes in 'cartitems',
-   * a new function will declared and the returning 
+   * a new function will declared and the returning
    * object {add, remove, list} will be a new object.
    */
   const add = useCallback((id: any) => {
     setCartItems((prev) => {
-        prev = prev!
+      prev = prev!
       const idx = prev.findIndex((ci) => ci.id == id)
       if (idx === -1) {
         return [...prev, { id: id, quantity: 1 }]
@@ -66,13 +68,13 @@ export default function useCartItems() {
   }, [])
 
   const remove = useCallback((id: any) => {
-    setCartItems(prev => prev!.filter(p => p.id != id))
+    setCartItems((prev) => prev!.filter((p) => p.id != id))
   }, [])
 
   const removeByQty = useCallback((id: any, qty: number) => {
-    setCartItems(prev => {
+    setCartItems((prev) => {
       prev = prev!
-      const idx = prev.findIndex(p => p.id === id)
+      const idx = prev.findIndex((p) => p.id === id)
       if (idx === -1) return prev
 
       const copy = [...prev]
@@ -90,8 +92,11 @@ export default function useCartItems() {
   // note: the returning object made up of list, add, remove, etc should be memoized so that users of this hook doesn't get a new object unless list, add, remove, etc *actually* changed.
   let list = cartItems
   if (cartItems == null) list = []
-  console.log('--- usecartitems: id-1, qty-', list?.find(l => l.id == 1)?.quantity)
-  const result = {list, add, remove, removeByQty}
+  console.log(
+    "--- usecartitems: id-1, qty-",
+    list?.find((l) => l.id == 1)?.quantity,
+  )
+  const result = { list, add, remove, removeByQty }
   // note: using Object.values() would be a horrible mistake.
   //return useMemo(() => result, [...Object.values(result)])
   return useMemo(() => result, [list, add, remove, removeByQty])
--- stores/useGlobalState.ts
+++ stores/useGlobalState.ts
@@ -1,4 +1,4 @@
-import {create} from 'zustand'
+import { create } from "zustand"
 
 type GlobalState = {
   cartItemsCount: number
@@ -7,9 +7,12 @@ type GlobalState = {
   setCartModalOpen: (value: boolean) => void
 }
 
-export const useGlobalState = create<GlobalState>((set ) => ({
-    cartItemsCount: 0,
-    setCartItemsCount:  (value: number) => set({cartItemsCount: value}),
-    isCartModalOpen: false,
-    setCartModalOpen: (value:boolean) => set({isCartModalOpen: value})
-} as const))
+export const useGlobalState = create<GlobalState>(
+  (set) =>
+    ({
+      cartItemsCount: 0,
+      setCartItemsCount: (value: number) => set({ cartItemsCount: value }),
+      isCartModalOpen: false,
+      setCartModalOpen: (value: boolean) => set({ isCartModalOpen: value }),
+    }) as const,
+)
