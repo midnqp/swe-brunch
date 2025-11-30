e178545e661c02bb99c7bc5e862c356e6a78f211
midnqp <midnqp@gmail.com>
2025-11-30 22:54:19 +0600

    feat: added software engineers menu

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import IconMuis from "@/components/IconMuis"
 import { PageLayout } from "@/components/PageLayout"
 import useCartItems from "@/hooks/useCartItems"
 import { useGlobalState } from "@/stores/useGlobalState"
+import backendApis from "@/utils/apis"
 import { IconButton, useMediaQuery, useTheme } from "@mui/material"
 import clsx from "clsx"
 import Image from "next/image"
@@ -17,11 +18,13 @@ export default function BrowsePage() {
     description:
       "lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet",
   }
-  const items = [
+  const _items = [
     food,
     { ...food, id: 2, name: "Item 2", price: 49.99 },
     { ...food, id: 3, name: "Item 3", price: 149.99 },
   ]
+
+  const products = backendApis.listEngineersItems()
 
   const cartItems = useCartItems()
   const globalStateCartItemsCount = useGlobalState((s) => s.cartItemsCount)
@@ -34,10 +37,43 @@ export default function BrowsePage() {
     console.log("BrowsePage :: cartItems changed")
   }, [cartItems])
 
+  const menus = [{
+    name: "Software Engineer's Brunch",
+    description: 'For those who write code. Meals optimized for protein and sustained anchoring in the desk.',
+    products: products
+  },
+  {
+    name: "Product Manager's Brunch",
+    description: `'High-impact' and 'customer-centric' cuisines. If your team has delivered before deadline, treat them with an 1:6.`,
+    products: products
+  },
+  {
+    name: "Founder's Brunch",
+    description: 'Wide variety of choices. Pick yours based on your remaining runway.',
+    products: products
+  }
+]
+
   return (
     <PageLayout>
-      <h1 className="mb-8 text-4xl">Browse</h1>
+      <h1 className="mt-8 text-4xl">Browse Your Taste</h1>
+      
+      {menus.map (m => <div className="mt-24"><div className="mb-8">
+        <h2 className=" text-gray-600">{m.name}</h2>
+        <div className="text-gray-400 text-sm"> {m.description} </div>
+      </div>
 
+      <div
+        className={clsx(
+          "mb-4 grid grid-cols-1 gap-4 md:grid-cols-4",
+          false && "justify-items-center",
+        )}
+      >
+        {m.products.map((item, idx) => (
+          <Card key={idx} {...{ cartItems, addToCart, item }} />
+        ))}
+      </div> </div>)}
+      
       {shouldShowViewCartBar && (
         <MobileViewCartBar
           totalLabel="Tk 200"
@@ -46,13 +82,6 @@ export default function BrowsePage() {
           }}
         />
       )}
-
-      {/** tip: ideal layout for item menus */}
-      <div className="mb-4 grid grid-cols-1 gap-4 md:grid-cols-3">
-        {items.map((item, idx) => (
-          <Card key={idx} {...{ cartItems, addToCart, item }} />
-        ))}
-      </div>
 
       {shouldShowViewCartBar && <MobileViewCartBarOffset />}
     </PageLayout>
@@ -72,57 +101,93 @@ function Card(props: any) {
   const isAdded = !!ci
   const [hover, setHover] = useState(false)
   const [isFavorited, setFavorited] = useState(false)
+  const isMobile = useMediaQuery(useTheme().breakpoints.down("md"))
 
   const onAddClick = (id: any) => {
     props.addToCart(id)
+    /*if (isMobile) {
+      setHover(true)
+      setTimeout(() => {
+        setHover(false)
+      }, 1000)
+    } */
     //setIsAdded(true)
   }
   const onFavoriteClick = (id: any) => {
-    // todo: handle
+    const favoritedItems = JSON.parse(
+      window.localStorage.getItem("favoritedItems") || "[]",
+    )
+    if (isFavorited) {
+      favoritedItems.splice(favoritedItems.indexOf(id), 1)
+    } else {
+      favoritedItems.push(id)
+    }
+    window.localStorage.setItem(
+      "favoritedItems",
+      JSON.stringify(favoritedItems),
+    )
     setFavorited((prev) => !prev)
   }
 
   const shouldShowItemQty = isAdded && !hover
 
+  useEffect(() => {
+    const favoritedItems = JSON.parse(
+      window.localStorage.getItem("favoritedItems") || "[]",
+    )
+    setFavorited(favoritedItems.includes(item.id))
+  }, [])
+
   return (
-    <div className="rounded-md border border-gray-300 bg-white p-4">
+    <div className="overflow-hidden rounded-md border border-gray-300 bg-white">
       <div style={{}} className="relative">
-        <IconButton
-          onMouseEnter={() => setHover(true)}
-          onMouseLeave={() => setHover(false)}
-          onClick={() => onAddClick(item.id)}
-          className={clsx(
-            "absolute! top-0 right-0 z-10 h-8 w-8 rounded-full",
-            shouldShowItemQty ? "bg-black!" : "bg-gray-300!",
-          )}
-        >
-          {shouldShowItemQty ? (
-            <span className="text-base! text-white!">
-              {ci.quantity}
-            </span> 
-          ): <IconMuis className="text-white!" iconName={"add"} />}
-        </IconButton>
-        <IconButton
-          onClick={() => onFavoriteClick(item.id)}
-          className="absolute! top-0 right-0 z-10 mr-10! h-8 w-8 rounded-full bg-gray-300!"
-        >
-          <IconMuis
-            className={clsx({
-              "muis-icon-filled text-white!": isFavorited,
-              "text-white!": !isFavorited,
-            })}
-            iconName="favorite"
-          />
-        </IconButton>
+        <div className="absolute! top-0 right-0 z-10 flex gap-2 p-4 md:pt-2 md:pr-2">
+          <IconButton
+            onClick={() => onFavoriteClick(item.id)}
+            className="h-10 w-10 rounded-full bg-gray-300! md:h-8 md:w-8 "
+          >
+            <IconMuis
+              className={clsx("", {
+                "muis-icon-filled text-red-600!": isFavorited,
+                "text-white!": !isFavorited,
+              })}
+              iconName="favorite"
+            />
+          </IconButton>
+          <IconButton
+            // note: using onMouseEnter fails miserably on mobile.
+            onPointerEnter={() => setHover(true)}
+            onPointerLeave={() => setHover(false)}
+            onClick={() => onAddClick(item.id)}
+            className={clsx(
+              "h-10 w-10 rounded-full md:h-8 md:w-8 shadow-2xl! ",
+              shouldShowItemQty ? "bg-black!" : "bg-gray-300! ",
+            )}
+          >
+            {shouldShowItemQty ? (
+              <p className="text-base! text-white!">{ci.quantity}</p>
+            ) : (
+              <IconMuis className="text-white!" iconName={"add"} />
+            )}
+          </IconButton>
+        </div>
       </div>
       {/** tip: how to work with next's image compo. */}
-      <div className="relative h-48 w-full">
-        <Image fill className="object-contain" alt="" src={item.image} />
+      <div className="relative aspect-[4/3] w-full!">
+        <Image
+          draggable={false}
+          fill
+          className="object-cover"
+          alt=""
+          src={item.image}
+        />
       </div>
-      <div>
+      <div className="p-4">
         <p className="font-bold">{item.name}</p>
-        <p>${item.price}</p>
-        <p>{item.description}</p>
+        <p className="mb-2">Tk {item.price}</p>
+        <p className="text-sm leading-tight text-gray-500">
+          {item.description}
+        </p>
       </div>
     </div>
   )
